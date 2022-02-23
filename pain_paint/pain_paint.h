13c07131b0cdde603f899cc34143cdb1a3c922db
WhoReadThisWillDie <78028560+WhoReadThisWillDie@users.noreply.github.com>
2022-02-24 01:55:45 +0300

    Add files via upload

--- pain_paint/pain_paint.h
+++ pain_paint/pain_paint.h
@@ -6,9 +6,11 @@
 using namespace std;
 
 //additional
-long long itc_len(string str);
+int len(string str);
 bool itc_find_str(string str1, string str2);
 void print_vector(const vector <string>& vector1);
+int str_to_int(string str);
+vector<string> split(string str);
 
 //check
 bool check_path(string str, string& path);
@@ -16,3 +18,6 @@ bool check_file(string path);
 
 //read
 void read_file(vector <string>& commands, string& path);
+
+//parse
+void parse(vector <string>& commands);
