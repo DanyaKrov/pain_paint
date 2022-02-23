82088d36928c6340a770c768640cd6d4232cb597
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2022-02-24 01:55:58 +0300

    Update mini_paint.cpp

--- pain_paint/mini_paint.cpp
+++ pain_paint/mini_paint.cpp
@@ -1,15 +1,16 @@
-﻿#include "pain_paint.h"
+#include "pain_paint.h"
 
 int main(int argc, string* argv[]) 
 {
 	string str;
 	string path;
 	vector <string> commands;
+	vector<vector<string>> matrix; //drawing
 	cout << "Input file path:" << endl;
 	getline(cin, str);
 	if (check_path(str, path) && check_file(path)) {
 		read_file(commands, path);
-		print_vector(commands);
+		parse(commands, matrix);
 	}
 	return 0;
 }
