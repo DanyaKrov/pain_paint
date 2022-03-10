e5169337031c335c39be402c985cf8237f9182d8
WhoReadThisWillDie <78028560+WhoReadThisWillDie@users.noreply.github.com>
2022-03-10 10:18:56 +0300

    ЛИНИЯ ГОТОВА

--- pain_paint/line.cpp
+++ pain_paint/line.cpp
@@ -3,6 +3,10 @@
 
 void line(vector<vector<string>>& matrix, int x1, int y1, int x2, int y2, string char_)
 {
+    if ((x1 < 0 || y1 < 0) || (x1 > matrix[0].size() || y1 > matrix.size())) {
+        print_error("Error: wrong parameters");
+        exit(-1);
+    }
     int length = max(abs(x2 - x1), abs(y2 - y1));
     double dx = (x2 - x1) / (double)length, dy = (y2 - y1) / (double)length, x = x1, y = y1;
     if (length > 0) {
