1ca99318ce13a737959593e2a5b37641171c99b8
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2022-03-17 23:41:50 +0300

    НИКАКОЙ ПЕРЕСТРОЙКИ!!!

---
+++ pain_paint/Game.cpp
@@ -0,0 +1,69 @@
+#include "Game.h"
+#include "TextureManager.h"
+#include "GameObject.h"
+#include "Map.h"
+
+GameObject* hero;
+SDL_Renderer* Game::renderer = nullptr;
+Map* map;
+Game::Game()
+{}
+Game::~Game()
+{}
+
+
+void Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreeen)
+{
+	int flags = 0;
+	if (fullscreeen)
+		flags = SDL_WINDOW_FULLSCREEN;
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+		if (window) {
+
+		}
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+		if (renderer) {
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		}
+		isRunning = true;
+	}
+	else
+		isRunning = false;
+	hero = new GameObject("morgenshtern/frames/frame_0.png", 0, 0, false, false); //player
+	map = new Map();
+}
+
+void Game::handleEvevents()
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type) {
+	case SDL_QUIT:
+		isRunning = false;
+		break;
+	}
+	if (GetKeyState(VK_SPACE) & 0x8000)
+		hero->jump();
+}
+
+void Game::update()
+{
+	count++;
+	hero->Update();
+}
+
+void Game::render()
+{
+	SDL_RenderClear(renderer);
+	map->DrawMap();
+	hero->Render();
+	SDL_RenderPresent(renderer);
+}
+void Game::clean()
+{
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+	cout << "Game cleaned" << endl;
+}
---
+++ pain_paint/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include "SDL.h"
+#include "SDL_image.h"
+#include <windows.h>
+using namespace std;
+
+class Game {
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreeen);
+	void handleEvevents();
+	void update();
+	void render();
+	void clean();
+
+	static SDL_Renderer *renderer;
+
+	bool running() {return isRunning;};
+private:
+	int count = 0;
+	bool isRunning;
+	SDL_Window* window;
+};
